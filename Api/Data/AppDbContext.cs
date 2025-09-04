0e3f607363e68cd96246d998af3bce1c111a7fc5
BassHM <al233324@edu.uaa.mx>
2025-09-03 20:17:08 -0600

    P1 - Add first EF migration

---
+++ Api/Data/AppDbContext.cs
@@ -0,0 +1,8 @@
+using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data;
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,6 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+});
 
 var app = builder.Build();
 
@@ -21,5 +29,6 @@ if (app.Environment.IsDevelopment())
 // app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
