0a1256c6609bd5f54c4fc6f69340817791690477
BassHM <al233324@edu.uaa.mx>
2025-09-08 07:56:03 -0600

    P1 - Add http client to SPA and Add CORS policy to API

--- Api/Program.cs
+++ Api/Program.cs
@@ -9,13 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
 });
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins(
+        "http://localhost:4200",
+        "https://localhost:4200"
+    ));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Client');
+export class App  implements OnInit {
+
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7276/api/members').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the http request')
+
+    });
+  }
 }
