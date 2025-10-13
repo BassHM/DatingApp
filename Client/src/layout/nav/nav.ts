85b8b6ef9bde2c007e2eab7c522cbe1ded186f90
BassHM <al233324@edu.uaa.mx>
2025-10-13 07:36:36 -0600

    P2 - Add Angular Conditionals to Display DOM Elements

--- Client/src/layout/nav/nav.ts
+++ Client/src/layout/nav/nav.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountServices } from '../../app/core/services/account-services';
 @Component({
@@ -10,16 +10,22 @@ import { AccountServices } from '../../app/core/services/account-services';
 export class Nav {
   protected credentials: any = {};
   protected accountService = inject(AccountServices)
+  protected loggedIn = signal(false);
 
   login(): void {
     console.log(this.credentials);
     this.accountService.login(this.credentials).subscribe({
       next: response => {
         console.log(response);
+        this.loggedIn.set(true);
       },
       error: error => {
         alert(error.message);
       }
     });
+  }
+
+  logout(): void { 
+    this.loggedIn.set(false);
   }
 }
