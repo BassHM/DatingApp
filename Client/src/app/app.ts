681b804434ee49d756027be4e800bd26c20cacd0
BassHM <al233324@edu.uaa.mx>
2025-10-22 11:41:13 -0600

    P2 - Add routes with view transitions

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,45 +1,12 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
 import { Nav } from '../layout/nav/nav';
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
-import { Home } from '../features/home/home';
 import { Router, RouterOutlet } from '@angular/router';
-import { NgClass } from '@angular/common';
-import { ToastService } from '../core/services/toast-service';
 @Component({
   selector: 'app-root',
-  imports: [Nav, RouterOutlet, NgClass],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App  implements OnInit {
+export class App {
   protected router = inject(Router);
-  private http = inject(HttpClient);
-  protected readonly title = signal('Dating App');
-  protected members = signal<User[]>([]);
-  private accountService = inject(AccountService);
-
-  async ngOnInit(): Promise<void> {
-    this.setCurrentUser();
-    this.members.set(await this.getMembers());
-
-  }
-
-  setCurrentUser(): void{
-    const userString = localStorage.getItem("user");
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers(): Promise<User[]> {
-    try {
-      return lastValueFrom(this.http.get<User[]>('http://localhost:5057/api/members'))
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
 }
