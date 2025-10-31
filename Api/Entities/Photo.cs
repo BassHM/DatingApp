f94a4f3144d0483e45286a7cb43af5059a083805
BassHM <al233324@edu.uaa.mx>
2025-10-31 17:51:17 -0600

    P2 - Add Member Migration

---
+++ Api/Entities/Photo.cs
@@ -0,0 +1,12 @@
+namespace Api.Entities
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public required string Url { get; set; }
+        public string? PublicId { get; set; }
+
+        // Navigation properties
+        public Member Member { get; set; } = null!;
+    }
+}
